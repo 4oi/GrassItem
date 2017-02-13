7abd20fa5aefaf0bea579f679ca50bc1e64fb1cb
明地えしら <ecilavip@outlook.jp>
2017-02-14 03:16:59 +0900

    [add] initial comimt

---
+++ src/main/java/life/grass/grassitem/GrassItem.java
@@ -0,0 +1,17 @@
+package life.grass.grassitem;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class GrassItem extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+//        this.getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
